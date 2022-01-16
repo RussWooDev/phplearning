4dd1e981830382265d7f90941c5866a25fe6185b
RussWooDev <79846904+RussWooDev@users.noreply.github.com>
2022-01-16 17:07:31 +0800

    Create phpcalculator_index.php

---
+++ phpcalculator_index.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h3>Addition</h3>
+<form action="addition.php" method="get">
+First Number: <input type ="number" name="add_one"><br>
+Second Number: <input type ="number" name="add_two"><br>
+<button>submit</button>
+
+</form>
+
+<h3>Division</h3>
+<form action="division.php" method="get">
+First Number: <input type ="number" name="divide_one"><br>
+Second Number: <input type ="number" name="divide_two"><br>
+<button>submit</button>
+</form>
+
+
+<a href="index.php">Reset</a>
+</body>
+</html>
